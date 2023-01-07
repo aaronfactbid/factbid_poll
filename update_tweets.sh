c7025eb2b58ddac551d5f4f4ec8bf0cb016f4f1f
aaronfactbid <aaron@factbid.org>
2023-01-07 11:00:52 -0300

    update the cronjob

--- update_tweets.sh
+++ update_tweets.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 dt=$(date '+%d/%m/%Y %H:%M:%S');
-echo "starting: $dt" >> /var/log/factbid/cronjob.log
+echo -e "starting: $dt\n" >> /var/log/factbid/cronjob.log
 curl -X POST "http://localhost:3000/search_tweets" -o /var/log/factbid/cronjob.log >> /var/log/factbid/cronjob.log
-echo "ended $? : $dt" >> /var/log/factbid/cronjob.log
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo -e "ended $? : $dt\n" >> /var/log/factbid/cronjob.log
